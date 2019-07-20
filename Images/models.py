d885753d37af847758969926931d0c061f17d2de
Omtatah <murtallahomtatah@gmail.com>
2019-07-20 15:03:26 +0300

    define the Location model that shall be used to tell the location a photo was taken in.

--- Images/models.py
+++ Images/models.py
@@ -1,3 +1,23 @@
+import datetime as dt
 from django.db import models
-
 # Create your models here.
+
+class Location(models.Model):
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.location
+
+    class Meta:
+        ordering = ['location']
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def delete_location(cls,location):
+        cls.objects.filter(location=location).delete()
+
+    @classmethod
+    def delete_category(cls,name):
+        cls.objects.filter(name = name).delete()
