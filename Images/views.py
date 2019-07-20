c738c7a9807ea09eb1dddef227ab6314838e8d2f
Omtatah <murtallahomtatah@gmail.com>
2019-07-20 15:10:58 +0300

    define the views of the app

--- Images/views.py
+++ Images/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http  import HttpResponse,Http404
+from django.shortcuts import render,redirect
+from .models import Image
+import datetime as dt
 
 # Create your views here.
+def welcome(request):
+    images=Image.objects.all()
+    return render(request,'welcome.html',{"images":images})
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        return render(request,'search.html',{"message":message,"images":searched_images})
+
+    else:
+        message="You haven't searched for any term"
+        return render(request,'search.html',{"message":message})
