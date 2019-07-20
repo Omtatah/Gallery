d85037a8fe3bfd82dd34cd2891eac64e4e18b128
Omtatah <murtallahomtatah@gmail.com>
2019-07-20 15:05:32 +0300

    define the Image model that facilitates the uploading of images to the app

--- Images/models.py
+++ Images/models.py
@@ -2,6 +2,32 @@ import datetime as dt
 from django.db import models
 # Create your models here.
 
+class Image(models.Model):
+    title=models.CharField(max_length=60)
+    categories = models.ManyToManyField(categories)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/',default='DEFAULT VALUE')
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def save_image(self):
+        self.save()
+
+
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        # images = cls.objects.filter(categories__icontains=search_term)
+        images = cls.objects.filter(categories__category=search_term) 
+
+        return images
+
 class Location(models.Model):
     location = models.CharField(max_length=100)
 
